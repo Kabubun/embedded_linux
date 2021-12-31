91918b71a3bc33b7c909a74770878d9b0ffe6ef4
Kabubun <kabukabu1418@gmail.com>
2021-12-31 21:58:43 +0900

    add cpp

---
+++ Cpp/raspi.cmake
@@ -0,0 +1,16 @@
+set(ARM_COMPILE_OPTION "-mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${ARM_COMPILE_OPTION}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${ARM_COMPILE_OPTION}")
+
+# cross compiler settings
+set(CMAKE_CROSSCOMPILING TRUE)
+set(CROSS_COMPILE "arm-linux-gnueabihf-")
+set(CMAKE_C_COMPILER ${CROSS_COMPILE}gcc)
+set(CMAKE_CXX_COMPILER ${CROSS_COMPILE}g++)
+set(CMAKE_LINKER ${CROSS_COMPILE}gcc)
+
+# root path settings
+set(CMAKE_FIND_ROOT_PATH ~/tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian-x64)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
